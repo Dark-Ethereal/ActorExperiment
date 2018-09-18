54e9efecb4aa9390f16adf2d2839b02f28906297
Dark-Ethereal <darkethereal@googlemail.com>
2018-09-18 14:00:06 +0100

    Started again
    Everything was wrong.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
-import PostOffice
+import Data.Text
+import Control.Monad.State.Lazy
+import Control.Monad.Writer.Lazy
+import Data.IntMap.Strict
+import Control.Lens
+
+data World = World (IntMap Entity) Turns
+
+data Entity = Entity
+
+type EntRef = Lens' (IntMap Entity) (Maybe Entity)
+
+type Turns = Int
+
+type Effect = StateT World (Writer Readout)
+
+type Reaction a b = a -> Effect b
+
+type Readout = [Text]
+
+newtype EntId = EntId Int deriving (Eq)
+
+
+
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
