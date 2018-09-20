46e76975846789187ef53317837a4e6c0b4fcc03
Dark-Ethereal <darkethereal@googlemail.com>
2018-09-20 12:41:02 +0100

    Removed redundant lib contents

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,32 +4,13 @@ module Lib
     ( someFunc
     ) where
 
-import Data.Text
-import Control.Monad.State.Lazy
-import Control.Monad.Writer.Lazy
-import Data.IntMap.Strict
-import Control.Lens
+import           Control.Lens
+import           Control.Monad.State.Lazy
+import           Control.Monad.Writer.Lazy
+import           Data.IntMap.Strict
 
-data World = World (IntMap Entity) Turns
-
-data Entity = Entity
-
-type EntRef = Lens' (IntMap Entity) (Maybe Entity)
-
-type Turns = Int
-
-type Effect = StateT World (Writer Readout)
-
-type Reaction a b = a -> Effect b
-
-type Readout = [Text]
-
-newtype EntId = EntId Int deriving (Eq)
-
-
-
-
+import           Classes
+import           Types
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
-
